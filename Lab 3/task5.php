708512f2391fd96b249ba4dfdad7cb4bd6b964dc
Fahim <faahim1278@gmail.com>
2021-06-13 16:40:32 +0600

    lab 3

---
+++ Lab 3/task5.php
@@ -0,0 +1,26 @@
+<?php
+	$backColor="green";
+	$fontColor="red";
+	$imageWidth="200px";
+	$imageHeight="250px";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>
+				<img src = "aiub.png" width="<?php echo $imageWidth ?>" heigth = "<?php echo $imageHeigth ?>" >
+			</td>
+			<td>
+				<h2 style="color: <?php echo $fontColor ?>;background-color: <?php echo $backColor ?>">American International<br> University-Bangladesh</h2>
+			</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
